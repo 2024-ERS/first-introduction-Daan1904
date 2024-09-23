1bfbf6b155961d170f4f891c272582799a9b036b
Daan <d.kroeze.1@student.rug.nl>
2024-09-23 11:07:49 +0200

    Classroom exercise

--- r-scripts/14-generalized linear models - Junner Koeland.R
+++ r-scripts/14-generalized linear models - Junner Koeland.R
@@ -9,6 +9,9 @@ library(lmerTest)     # for tests of significance of mixed-effects models
 library(multcompView)
 library(emmeans)
 
+# use this dataset (remove # to use line)
+#browseURL("https://docs.google.com/spreadsheets/d/1pZzz337HcuQ28i7sIZu64JWAah9W77fI3hp0I9StZHA/edit?gid=0#gid=0")
+
 dat<-readr::read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3XEZfbq9FoQZ2r1d4XBzCd7Dbn4Hh4IKphhJhKqQtoRQ0cNwLxjx_U3ZOlOfpUvm_ADHgzRf6wFks/pub?gid=0&single=true&output=csv") 
 
 
@@ -27,16 +30,32 @@ dat %>% ggplot(aes(specrich)) +
 # if plant species richness is different between the treatments 
 # and inspect the Q-Q plot of the residuals, and do a Shapiro test for normality 
 # do a one-way anova ( 1 predictor with >2 categories)
+m1 <- lm(specrich ~ graztreat, data = dat)
+anova(m1)
+# same thing
+m1 <- aov(specrich ~ graztreat, data = dat)
+anova(m1)
 
 
 # use tukey test to see which groups are different
+# is a post hoc test
+tukey_results <- TukeyHSD(m1)
+tukey_results
 
+# I can add letters and for C -V - R they are a b c
+# means that with the same letter are not significantly different
+tukey_cld <- multcompView::multcompLetters(TukeyHSD(m1)$graztreat[, "p adj"])
+tukey_cld
 
 # explore the q-q plot of the residuals of the model to check normality
-
+plot(m1, which = 2)
 
 # plot the histogram of the residuals with normal curve
-
+g <- m1$residuals
+m <- mean(g)
+std <- sd(g)
+hist(g, prob = TRUE)
+curve(dnorm(x, mean = m, sd = std), col = "blue", lwd = 2, add = TRUE, yaxt = "n")
 
 # run the standard linear model as a generalized linear model
 
